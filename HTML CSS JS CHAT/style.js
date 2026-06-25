e8c42d3ce9a5a941bf099757a2cb6d99de3e9cd0
simgit101 <simran11.suhani@gmail.com>
2026-06-25 07:05:47 +0530

    Add files via upload

---
+++ HTML CSS JS CHAT/style.js
@@ -0,0 +1,30 @@
+const messageBox = document.querySelector("#message-box");
+const sendButton = document.querySelector("#send-button");
+const messageListArea = document.querySelector("#messagelist-area");
+
+function NewMessage() {
+    if (messageBox.value.trim() === "") {
+        return;
+    }
+    const MessageBubble = document.createElement("article");
+    MessageBubble.className="message sent"
+
+    const messageParagraph = document.createElement("p");
+    messageParagraph.textContent = messageBox.value.trim();
+
+    const time = document.createElement("small");
+    time.textContent = new Date().toLocaleTimeString()
+    MessageBubble.appendChild(messageParagraph);
+    MessageBubble.appendChild(time);
+    messageListArea.appendChild(MessageBubble);
+    messageBox.value = "";
+    MessageBubble.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+sendButton.addEventListener("click", NewMessage);
+messageBox.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        NewMessage();
+    }
+});
